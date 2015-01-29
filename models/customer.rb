0e8102dc1a96b98ead70df673c591e67adc41717
Kyle Geske <stungeye@gmail.com>
2015-01-29 16:07:14 -0600

    Modified the customer to use the new style validations.

--- models/customer.rb
+++ models/customer.rb
@@ -1,12 +1,5 @@
 class Customer < ActiveRecord::Base
   # This AR object is linked with the customers table.
-  
-  # Customer have a many to one relationship with a province by
-  # way of a province_id foreign key. In other words, a customer
-  # belongs to a specific province.
-  belongs_to :province
-  
-  # If any of these three columns are blank then we cannot persist
-  # the customer to the database.
-  validates_presence_of :name, :email, :city
+  validates :email, uniqueness: true
+  validates :name, :email, presence: true
 end
