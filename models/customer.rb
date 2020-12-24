62285ac7f086184b425c65af0b1bd540c2bbf071
Kyle Geske <stungeye@gmail.com>
2020-12-24 12:52:41 -0600

    Update customer.rb

--- models/customer.rb
+++ models/customer.rb
@@ -1,4 +1,6 @@
 class Customer < ActiveRecord::Base
+  # This AR class is linked with the customers table.
+  # This table has a province_id foreign key so a customer:
   belongs_to :province
 
   validates :email, uniqueness: true
