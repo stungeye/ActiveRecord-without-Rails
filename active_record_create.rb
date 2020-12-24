90497a964e597abdcaa41423846021da2e348b89
Kyle Geske <stungeye@gmail.com>
2020-12-24 12:56:42 -0600

    Update active_record_create.rb

--- active_record_create.rb
+++ active_record_create.rb
@@ -29,9 +29,9 @@ puts new_customer.inspect
 
 # This time we set the required properties as key/value arguments of the new method.
 
-wally = Customer.new( :name  => "Wally Glutton",
-                      :email => "wglutton@mailinator.com",
-                      :city  => "Winnipeg")
+wally = Customer.new( name:  "Wally Glutton",
+                      email: "wglutton@mailinator.com",
+                      city:  "Winnipeg" )
 
 # Like the previous example we need to call save to persist the object
 # to the customers table.
@@ -40,14 +40,14 @@ wally.save
 
 # Method Three: Create the object and persist it all at once.
 
-jane = Customer.create( :name  => "Jane Smith",
-                        :email => "jsmith@mailinator.com",
-                        :city  => "Winnipeg" )
+jane = Customer.create( name:  "Jane Smith",
+                        email: "jsmith@mailinator.com",
+                        city:  "Winnipeg" )
 
 # Let's test the validations we added to the models/customer.rb file:
 # validates_presence_of :name, :email, :city
 
-bobby = Customer.new( :name => "Bobby McGee") # Purposefully missing the city and email.
+bobby = Customer.new( name: "Bobby McGee" ) # Purposefully missing the city and email.
 
 # Save will return false if any of our validations fail.
 if (bobby.save)
@@ -55,6 +55,7 @@ if (bobby.save)
   puts bobby.inspect
 else
   puts "There was an error saving bobby to the database."
+  
   # We can loop through all the validation errors.
   bobby.errors.messages.each do |column, errors|
     errors.each do |error|
